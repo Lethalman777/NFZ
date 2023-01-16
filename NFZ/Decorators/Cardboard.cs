a1cfe782ed24a525512653bf15de7b3dd908e674
Karzelus <D.Gramacki@wp.pl>
2023-01-16 14:23:20 +0100

    Decorator

---
+++ NFZ/Decorators/Cardboard.cs
@@ -0,0 +1,19 @@
+﻿using NFZ.Entities;
+
+namespace NFZ.Decorators
+{
+    public class Cardboard : Packaging
+    {
+        public Cardboard(Order order) : base(order) { }
+
+        public float Cena(Packaging packaging)
+        {
+            return packaging.Cena() + 10;
+        }
+
+        public String Opis(Packaging packaging)
+        {
+            return packaging.Opis() + "Karton";
+        }
+    }
+}
---
+++ NFZ/Decorators/Packaging.cs
@@ -0,0 +1,27 @@
+﻿using NFZ.Entities;
+
+namespace NFZ.Decorators
+{
+    public class Packaging : Order
+    {
+        private Order order { get; set; }
+        private float cena { get; set; }
+        private String opis { get; set; }
+        public Packaging(Order order)
+        {
+            this.order = order;
+            this.cena = 1;
+            this.opis = "Opakowanie";
+        }
+
+        public float Cena()
+        {
+            return 1;
+        }
+
+        public String Opis()
+        {
+            return "Opakowanie: ";
+        }
+    }
+}
